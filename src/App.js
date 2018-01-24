435bb03d5ee2166941a36e13440b60e82ffbd6ce
Victor Pino <victopin0@gmail.com>
2018-01-23 21:06:10 -0400

    feat(app): Add new component WeatherLocation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import WeatherLocation from './components/WeatherLocation';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <WeatherLocation></WeatherLocation>
       </div>
     );
   }
