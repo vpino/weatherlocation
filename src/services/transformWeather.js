28bbce27698dd1d38d3842dbede87c7849dfc87c
Victor Pino <victopin0@gmail.com>
2018-01-27 16:37:56 -0400

    feat(app): Add services to the project 'transformWeather'

---
+++ src/services/transformWeather.js
@@ -0,0 +1,29 @@
+import convert from 'convert-units';
+import { SUN } from './../constants/weathers';
+
+const getTemp = kelvin => {
+	return convert(kelvin).from('K').to('C').toFixed(2);
+}
+
+const getWeatherState = weatherState => {
+	return SUN;
+}
+	
+const transformWeather = weatherData => {
+		
+	const { humidity, temp } = weatherData.main;
+	const { speed } = weatherData.wind;
+	const { weatherState } = getWeatherState(this.weather);
+	const temperature = Number(getTemp(temp));
+
+	const data = {
+		humidity,
+		temperature,
+		weatherState,
+		wind: `${speed} m/s`,
+	}
+
+	return data;
+} 
+
+export default transformWeather;
