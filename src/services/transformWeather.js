796eb0b2a93b7648b814c52ca38af08d328c4959
Victor Pino <victopin0@gmail.com>
2018-01-27 16:55:59 -0400

    feat(app): Change the position the conversion to number

--- src/services/transformWeather.js
+++ src/services/transformWeather.js
@@ -2,7 +2,7 @@ import convert from 'convert-units';
 import { SUN } from './../constants/weathers';
 
 const getTemp = kelvin => {
-	return convert(kelvin).from('K').to('C').toFixed(2);
+	return Number(convert(kelvin).from('K').to('C').toFixed(2));
 }
 
 const getWeatherState = weatherState => {
@@ -14,7 +14,7 @@ const transformWeather = weatherData => {
 	const { humidity, temp } = weatherData.main;
 	const { speed } = weatherData.wind;
 	const { weatherState } = getWeatherState(this.weather);
-	const temperature = Number(getTemp(temp));
+	const temperature = getTemp(temp);
 
 	const data = {
 		humidity,
