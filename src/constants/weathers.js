f1d07505de6e6d63565b37a0986749eccabc1eef
Victor Pino <victopin0@gmail.com>
2018-01-27 21:36:24 -0400

    feat(app): Add APIURL constant

--- src/constants/weathers.js
+++ src/constants/weathers.js
@@ -5,3 +5,4 @@ export const RAIN = "rain";
 export const SNOW = "snow";
 export const WINDY = "windy";
 export const APIKEY = "b74b54ec96e56a50891249fc6ef8ab3e";
+export const APIURL = "http://api.openweathermap.org/data/2.5/weather"; 
